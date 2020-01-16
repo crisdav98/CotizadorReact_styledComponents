70ce8709ce21baacaa74b61af3481b100f01f8d8
crisdav98 <47307974+crisdav98@users.noreply.github.com>
2020-01-16 12:34:08 -0500

    Add files via upload

---
+++ src/components/Resumen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import {primeraMayuscula} from '../Helper';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #FFF;
+    margin-top: 1rem;
+
+`;
+
+const Resumen = ({datos}) => {
+
+    // extraer de datos
+    const {marca,year,plan} = datos;
+    if(marca==='' || year==='' || plan==='') return null;
+
+    return (
+        <ContenedorResumen>
+            <h2>Resumen de cotización</h2>
+            <ul>
+                <li>Marca: {primeraMayuscula(marca)}</li>
+                <li>Año: {primeraMayuscula(year)}</li>
+                <li>Plan: {primeraMayuscula(plan)}</li>
+            </ul>
+        </ContenedorResumen>
+    );
+};
+
+Resumen.propTypes = {
+    datos: PropTypes.object.isRequired
+}
+
+export default Resumen;
